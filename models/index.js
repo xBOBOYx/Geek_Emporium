d3133e1d1e6919d1923e971a5dcd6f129b97db0b
Anthony Zamora <84356810+xBOBOYx@users.noreply.github.com>
2021-09-14 20:57:36 -0700

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,13 +1,10 @@
 const User = require('./User');
-<<<<<<< HEAD
 const Item = require('./item');
 
 User.hasMany(Item, {
-=======
 const Item = require('./Item');
 
 User.hasMany(Project, {
->>>>>>> 18f0c343b3e5e828859f3728f9c441eb9f08afdc
   foreignKey: 'user_id',
   onDelete: 'CASCADE'
 });
