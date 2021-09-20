372fa779647926c7a7d438f2a939eb892a56ac0b
Aniesia Schmolesky <aniesiaschmolesky@gmail.com>
2021-09-19 19:16:41 -0700

    added Multer

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 const path = require('path');
 const express = require('express');
+const multer  = require('multer');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const helpers = require('./utils/helpers');
 const http = require('http');
 const util = require('util');
-const Formidable = require('formidable');
-const cloudinary = require("./config/cloudinary");
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+app.use(express.static(__dirname + '/views'));
+app.use('/uploads', express.static('uploads'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
@@ -37,6 +40,39 @@ const sess = {
     db: sequelize
   }),
 };
+
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './uploads')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname)
+  }
+})
+var upload = multer({ storage: storage })
+
+app.post('/profile-upload-single', upload.single('profile-file'), function (req, res, next) {
+  // req.file is the `profile-file` file
+  // req.body will hold the text fields, if there were any
+  console.log(JSON.stringify(req.file))
+  var response = '<a href="/">Home</a><br>'
+  response += "Files uploaded successfully.<br>"
+  response += `<img src="${req.file.path}" /><br>`
+  return res.send(response)
+})
+
+app.post('/profile-upload-multiple', upload.array('profile-files', 12), function (req, res, next) {
+  // req.files is array of `profile-files` files
+  // req.body will contain the text fields, if there were any
+  var response = '<a href="/">Home</a><br>'
+  response += "Files uploaded successfully.<br>"
+  for(var i=0;i<req.files.length;i++){
+      response += `<img src="${req.files[i].path}" /><br>`
+  }
+  
+  return res.send(response)
+})
 
 app.use(session(sess));
 
