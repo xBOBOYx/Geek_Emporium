adb4a043f7b8cb53c31eb2d06068c34a08868049
kirafaye99 <kirademarco99@gmail.com>
2021-11-06 10:45:05 -0700

    file structure

---
+++ client/src/index.js
@@ -0,0 +1,7 @@
+import ReactDOM from 'react-dom';
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+require('dotenv').config();
+
+ReactDOM.render(<App />, document.getElementById('root'));
